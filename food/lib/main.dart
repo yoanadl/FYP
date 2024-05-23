711f7964096e7bcfe3955d4570e368d6e8a545d7
ry0916 <ruishenyang01@gmail.com>
2024-05-23 12:34:25 +0800

    test1

--- food/lib/main.dart
+++ food/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
 
         body: Container(
           child: Text('test'),
-          color: Colors.red,
+          color: Colors.green,
         ),//trrrttr
         
 
