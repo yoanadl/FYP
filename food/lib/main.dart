c672f55775d6f57bf306e2f8d5d364ebe07ebcdf
yoanadl <reginayoan.adl@gmail.com>
2024-05-23 11:38:18 +0800

    test

---
+++ food/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+// import 'package:random_color/random_color.dart';
+
+
+
+
+
+void main (){
+    runApp( MyApp() );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // RandomColor _randomColor = RandomColor();
+    // Color _color = _randomColor.randomColor();
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text("Flutter is fun")
+        ),
+
+        body: Container(
+          child: Text('test'),
+        ),
+        
+
+
+
+      ),
+    );
+  }
+}
