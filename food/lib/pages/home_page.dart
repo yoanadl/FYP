404904b240bba5310e79e78ee6f92c1e354877a9
Sorarat Urairaksa <sorarat.urairaksa@gmail.com>
2024-06-09 21:21:18 +0700

    added the navbar to the home page

--- food/lib/pages/home_page.dart
+++ food/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:food/components/navbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +11,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // track the currently selected item
+  int _seletectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
+      ),
+
+      body: Center(
+        child: Text('Home Page Content'),
+      ),
+
+      bottomNavigationBar: Navbar(
+        currentIndex: _seletectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _seletectedIndex = index;
+          });
+        }
       ),
     );
   }
