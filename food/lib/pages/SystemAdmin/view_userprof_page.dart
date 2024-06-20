d5938e42357f1aa7f9d30ccf2aa748fb9740a1e0
yoanadl <reginayoan.adl@gmail.com>
2024-06-20 16:12:24 +0800

    delete dump main

--- food/lib/pages/SystemAdmin/view_userprof_page.dart
+++ food/lib/pages/SystemAdmin/view_userprof_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../workout_page.dart';
 import '../community_page.dart';
 import '../profile_page.dart';
 import '../home_page.dart';
