89150722d688cb9a8ae65603dce83ad1de4f2a96
yoanadl <reginayoan.adl@gmail.com>
2024-06-28 18:36:19 +0800

    wip sys admin ui

--- food/lib/pages/admin/admin_home_page.dart
+++ food/lib/pages/admin/admin_home_page.dart
@@ -11,86 +11,68 @@ class AdminHomePage extends StatelessWidget {
     return Container(
       color: Colors.white,
       //basic padding
-      padding: EdgeInsets.only(top: 40.0, right: 40.0, left: 24.0),
+      padding: EdgeInsets.only(top: 40.0, right: 24.0, left: 24.0),
 
       child: Column(
         children: [
           // AppBar content
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.only(bottom: 8.0), // Add some bottom padding if needed
-            child: AppBar(
-              automaticallyImplyLeading: false, // hide the back button
-              backgroundColor: Colors.white,
-              centerTitle: false,
-              elevation: 0, // Remove the shadow of the AppBar
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(0.0),
-                    child: Text(
-                      'Hello, Admin!',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
+          AppBar(
+            automaticallyImplyLeading: false, // hide the back button
+            backgroundColor: Colors.white,
+            title: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello, Admin!',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(0),
-                    child: Text(
-                      formattedDate,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-              actions: [
-                // notification icon
-                Padding(
-                  padding: EdgeInsets.all(0),
-                  child: Container(
-                    width: 36,
-                    height: 36,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.grey[300],
-                    ),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.notifications),
-                      iconSize: 20,
-                      color: Colors.black,
-                    ),
-                  ),
+                  overflow: TextOverflow.ellipsis,
                 ),
-                // Space between notification icon and profile icon
-                SizedBox(width: 16.0),
-                // profile icon with right padding
-                Padding(
-                  padding: const EdgeInsets.only(right: 24.0),
-                  child: SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.grey[300],
-                      ),
-                    ),
+                Text(
+                  formattedDate,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
                   ),
+                  overflow: TextOverflow.ellipsis,
                 ),
               ],
             ),
+            actions: [
+              // notification icon
+              Container(
+                width: 36,
+                height: 36,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[300],
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.notifications),
+                  iconSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              // Space between notification icon and profile icon
+              SizedBox(width: 16.0),
+              // profile icon with right padding
+              SizedBox(
+                width: 60,
+                height: 60,
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ),
+            ],
           ),
           // Body content
           Expanded(
@@ -103,17 +85,19 @@ class AdminHomePage extends StatelessWidget {
                     SizedBox(height: 36),
                     Container(
                       alignment: Alignment.centerLeft,
-                      child: Text(
-                        'User Accounts',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
+                      child: 
+                      
+                      Text(
+                          'User Accounts',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
                         ),
                       ),
-                    ),
 
-                    SizedBox(height: 16.0),
+                      SizedBox(height: 16.0),
 
                     Container(
                       width: 350,
@@ -124,10 +108,11 @@ class AdminHomePage extends StatelessWidget {
                       ),
 
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          SizedBox(height: 17),
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Container(
                                 width: 149,
@@ -140,34 +125,47 @@ class AdminHomePage extends StatelessWidget {
                                       width: 1.0,
                                   ),
                                 ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      'Number of Users',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'All Users',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
                                       ),
-                                    ),
-                                    Text(
-                                      '200',
-                                      style: TextStyle(
-                                        color: Colors.black,
+                                      Text(
+                                        '200',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 20,
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ),
+                              //between all users and active users
                               SizedBox(width: 10),
+
                               Container(
-                                width: 160,
-                                padding: EdgeInsets.all(15),
+                                width: 149,
+                                height: 95,
                                 decoration: BoxDecoration(
                                   color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10.0),
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  border: Border.all(
+                                      color: Color(0xFFD9D9D9), // Replace 0xFF00FF00 with your hex color code
+                                      width: 1.0,
+                                  ),
                                 ),
                                 child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
                                   crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
                                     Text(
@@ -175,12 +173,15 @@ class AdminHomePage extends StatelessWidget {
                                       style: TextStyle(
                                         color: Colors.black,
                                         fontWeight: FontWeight.bold,
+                                        fontSize: 14,
                                       ),
                                     ),
                                     Text(
                                       '150',
                                       style: TextStyle(
                                         color: Colors.black,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 20,
                                       ),
                                     ),
                                   ],
@@ -191,7 +192,18 @@ class AdminHomePage extends StatelessWidget {
                         ],
                       ),
                     ),
-                    SizedBox(height: 30),
+
+                    SizedBox(height: 48),
+
+                    Text(
+                      'User Profile',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
+                    ),
+
                     Container(
                       width: 350,
                       height: 330,
@@ -202,17 +214,6 @@ class AdminHomePage extends StatelessWidget {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.all(15),
-                            child: Text(
-                              'User Profile',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
                           SizedBox(height: 10),
                           Row(
                             children: [
