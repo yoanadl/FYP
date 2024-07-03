d1ba9edc52137d610f3fa68fa349aae3e3bac362
Sorarat Urairaksa <sorarat.urairaksa@gmail.com>
2024-07-03 11:27:55 +0800

    added the break timer

--- food/lib/pages/workout/workout_activity.dart
+++ food/lib/pages/workout/workout_activity.dart
@@ -1,3 +1,163 @@
+// import 'package:flutter/material.dart';
+// import 'dart:async';
+
+// class WorkoutActivityPage extends StatefulWidget {
+//   final String activityTitle;
+//   final int duration; // duration in minutes
+//   final int activityIndex;
+//   final List<String> activities;
+//   final List<int> durations;
+
+//   const WorkoutActivityPage({
+//     Key? key,
+//     required this.activityTitle,
+//     required this.duration,
+//     required this.activityIndex,
+//     required this.activities,
+//     required this.durations,
+//   }) : super(key: key);
+
+//   @override
+//   _WorkoutActivityPageState createState() => _WorkoutActivityPageState();
+// }
+
+// class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
+//   Timer? _timer;
+//   late int _remainingTime = widget.duration * 60; // convert minutes to seconds
+//   bool _isPaused = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _startTimer();
+//   }
+
+//   void _startTimer() {
+//     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+//       if (_remainingTime > 0 && !_isPaused) {
+//         setState(() {
+//           _remainingTime--;
+//         });
+//       } else if (_remainingTime == 0) {
+//         _timer?.cancel();
+//         if (widget.activityIndex < widget.activities.length - 1) {
+//           Navigator.pushReplacement(
+//             context,
+//             MaterialPageRoute(
+//               builder: (context) => WorkoutActivityPage(
+//                 activityTitle: widget.activities[widget.activityIndex + 1],
+//                 duration: widget.durations[widget.activityIndex + 1],
+//                 activityIndex: widget.activityIndex + 1,
+//                 activities: widget.activities,
+//                 durations: widget.durations,
+//               ),
+//             ),
+//           );
+//         } else {
+//           Navigator.pop(context);
+//         }
+//       }
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _timer?.cancel();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     double progress = _remainingTime / (widget.duration * 60);
+    
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 '${widget.activityTitle}',
+//                 style: TextStyle(
+//                   fontSize: 30, 
+//                   fontWeight: FontWeight.bold),
+//               ),
+//               SizedBox(height: 30),
+//               Stack(
+//                 alignment: Alignment.center,
+//                 children: [
+//                   SizedBox(
+//                     width: 150,
+//                     height: 150,
+//                     child: CircularProgressIndicator(
+//                       value: progress,
+//                       strokeWidth: 10,
+//                     ),
+//                   ),
+//                   Text(
+//                     '${_remainingTime ~/ 60}:${(_remainingTime % 60).toString().padLeft(2, '0')}',
+//                     style: TextStyle(
+//                       fontSize: 30, 
+//                       fontWeight: FontWeight.bold),
+//                   ),
+//                 ],
+//               ),
+//               SizedBox(height: 30),
+//               Text(
+//                 '12 kcal burnt',
+//                 style: TextStyle(fontSize: 18),
+//               ),
+//               Text(
+//                 'Your goal: 100 kcal',
+//                 style: TextStyle(fontSize: 18),
+//               ),
+//               SizedBox(height: 20),
+//               IconButton(
+//                 onPressed: () {
+//                   setState(() {
+//                     _isPaused = !_isPaused;
+//                   });
+//                 },
+//                 icon: Icon(
+//                   _isPaused ? Icons.play_arrow : Icons.pause,
+//                   size: 50,
+//                 ),
+//               ),
+//               SizedBox(height: 20),
+//               Container(
+//                 height: 50,
+//                 width: 220,
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.circular(10), // Customize the border radius as needed
+//                   color: Color(0xFFBA1200), // Red background color
+//                 ),
+//                 child: TextButton(
+//                   onPressed: () {
+//                     _timer?.cancel();
+//                     Navigator.pop(context);
+//                   },
+//                   child: Text(
+//                     'Stop Workout',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 18,
+//                     )
+//                   ),
+//                 )
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -23,8 +183,9 @@ class WorkoutActivityPage extends StatefulWidget {
 
 class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
   Timer? _timer;
-  late int _remainingTime = widget.duration * 60; // convert minutes to seconds
+  late int _remainingTime; // Remaining time in seconds
   bool _isPaused = false;
+  bool _isBreak = false;
 
   @override
   void initState() {
@@ -33,6 +194,12 @@ class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
   }
 
   void _startTimer() {
+    if (_isBreak) {
+      _remainingTime = 30; // 30-second break
+    } else {
+      _remainingTime = widget.duration * 60; // Duration in seconds
+    }
+
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       if (_remainingTime > 0 && !_isPaused) {
         setState(() {
@@ -40,21 +207,30 @@ class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
         });
       } else if (_remainingTime == 0) {
         _timer?.cancel();
-        if (widget.activityIndex < widget.activities.length - 1) {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => WorkoutActivityPage(
-                activityTitle: widget.activities[widget.activityIndex + 1],
-                duration: widget.durations[widget.activityIndex + 1],
-                activityIndex: widget.activityIndex + 1,
-                activities: widget.activities,
-                durations: widget.durations,
+        if (_isBreak) {
+          // Proceed to next activity
+          _isBreak = false;
+          if (widget.activityIndex < widget.activities.length - 1) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => WorkoutActivityPage(
+                  activityTitle: widget.activities[widget.activityIndex + 1],
+                  duration: widget.durations[widget.activityIndex + 1],
+                  activityIndex: widget.activityIndex + 1,
+                  activities: widget.activities,
+                  durations: widget.durations,
+                ),
               ),
-            ),
-          );
+            );
+          } else {
+            // Finish workout if all activities are completed
+            Navigator.pop(context);
+          }
         } else {
-          Navigator.pop(context);
+          // Start break timer
+          _isBreak = true;
+          _startTimer();
         }
       }
     });
@@ -68,7 +244,7 @@ class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
 
   @override
   Widget build(BuildContext context) {
-    double progress = _remainingTime / (widget.duration * 60);
+    double progress = 1 - (_remainingTime / (widget.duration * 60)); // Reverse progress for break time
     
     return Scaffold(
       backgroundColor: Colors.white,
@@ -82,10 +258,11 @@ class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                '${widget.activityTitle}',
+                _isBreak ? 'Break' : '${widget.activityTitle}',
                 style: TextStyle(
-                  fontSize: 30, 
-                  fontWeight: FontWeight.bold),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               SizedBox(height: 30),
               Stack(
@@ -102,8 +279,9 @@ class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
                   Text(
                     '${_remainingTime ~/ 60}:${(_remainingTime % 60).toString().padLeft(2, '0')}',
                     style: TextStyle(
-                      fontSize: 30, 
-                      fontWeight: FontWeight.bold),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ],
               ),
@@ -133,8 +311,8 @@ class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
                 height: 50,
                 width: 220,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), // Customize the border radius as needed
-                  color: Color(0xFFBA1200), // Red background color
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xFFBA1200),
                 ),
                 child: TextButton(
                   onPressed: () {
@@ -146,9 +324,9 @@ class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 18,
-                    )
+                    ),
                   ),
-                )
+                ),
               ),
             ],
           ),
@@ -157,3 +335,4 @@ class _WorkoutActivityPageState extends State<WorkoutActivityPage> {
     );
   }
 }
+
