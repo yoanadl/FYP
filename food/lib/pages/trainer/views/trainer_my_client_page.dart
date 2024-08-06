b0e134b1b5a1a4a099614a4c5a25df6cb31b2cd8
Yoan Liemanta <reginayoan.adl@gmail.com>
2024-08-06 13:02:28 +0800

    client list page

--- food/lib/pages/trainer/views/trainer_my_client_page.dart
+++ food/lib/pages/trainer/views/trainer_my_client_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'trainer_client_analytics_page.dart';
-import 'package:food/components/trainer_navbar.dart'; 
-import 'trainer_base_page.dart';
-import 'trainer_workout_plan_page.dart';
-import 'trainer_profile_page.dart';
 
 class TrainerMyClientPage extends StatelessWidget {
   @override
@@ -11,38 +7,84 @@ class TrainerMyClientPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        automaticallyImplyLeading: false, 
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
-        title: Text('My Clients', style: TextStyle(fontWeight: FontWeight.w600),),
-      ),
-      body: ListView.builder(
-        itemCount: 7, // Example client count
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Client client client #${index + 1}'),
-            trailing: PopupMenuButton<String>(
-              onSelected: (String result) {
-                if (result == 'View Analytics') {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TrainerClientAnalyticsPage()),
-                  );
-                }
-                // Handle menu selection
-              },
-              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: 'View Analytics',
-                  child: Text('View Analytics'),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'Delete Client',
-                  child: Text('Delete Client'),
-                ),
-              ],
+        title: const Padding(
+          padding: EdgeInsets.fromLTRB(16, 24, 16, 0),
+          child: Text(
+            'My Clients',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Poppins',
+              fontSize: 25,
             ),
-          );
-        },
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 24, 16, 0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search Client...',
+                    hintStyle: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey,
+                    ),
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true, // Ensures the ListView is as small as possible
+                itemCount: 7, // Example client count
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      'Client #${index + 1}',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                      ),
+                    ),
+                    trailing: PopupMenuButton<String>(
+                      onSelected: (String result) {
+                        if (result == 'View Analytics') {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TrainerClientAnalyticsPage(),
+                            ),
+                          );
+                        }
+                        // Handle menu selection
+                      },
+                      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                        const PopupMenuItem<String>(
+                          value: 'View Analytics',
+                          child: Text('View Analytics'),
+                        ),
+                        const PopupMenuItem<String>(
+                          value: 'Delete Client',
+                          child: Text('Delete Client'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
